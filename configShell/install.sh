c558f55eef8132a2f619c1689f44c0446cd17144
张博 <llkjzhangbo@bitbucket.org>
2018-09-25 09:57:14 +0800

    修改 docker 共享文件夹路径
    修改 docker 共享文件夹路径

--- configShell/install.sh
+++ configShell/install.sh
@@ -1,7 +1,6 @@
-docker run -it --name jurua-api-tomcat-0 -p 8000:8080 -v /usr/local/Cellar/tomcat/9.0.12/libexec/conf:/usr/local/tomcat/conf -v /Users/zhangbo/Documents/IDEAWorkSpace/jurua-api/build/libs:/usr/local/tomcat/webapps tomcat:9.0.12
-
+# tomcat
+docker run -it --name jurua-api-tomcat-0 -p 8000:8080 -v /usr/local/Cellar/tomcat/9.0.12/libexec/conf:/usr/local/tomcat/conf -v /usr/local/share/macBookShare/projects/api/jurua:/usr/local/tomcat/webapps tomcat:9.0.12
+# tomcat
 docker run -it --name jurua-api-tomcat-1 -p 8001:8080  --volumes-from jurua-api-tomcat-0 tomcat:9.0.12
-
-docker run -itd --name jurua-nginx -p 9000:80 -v /usr/local/etc/nginx:/etc/nginx -v /Users/zhangbo/Documents/WebStormWorkSpace/distProjects/jurua:/usr/share/nginx/html nginx
-
-docker run -itd --name jurua-nginx -p 9000:80 nginx
+# nginx
+docker run -itd --name jurua-nginx -p 9000:80 -v /usr/local/etc/nginx:/etc/nginx -v /usr/local/share/macBookShare/projects/web/jurua:/usr/share/nginx/html nginx
